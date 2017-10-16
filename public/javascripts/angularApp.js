dafd366d9206df71af5a59052b3bf3253a11c0f8
Henry Barnacle <henrybarnacle@yahoo.com>
2017-10-16 07:04:51 -0400

    adding front end

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,77 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+		$urlRouterProvider.otherwise('home');
+	}]);
+
+app.factory('posts', [function() {
+	var o = {
+		posts:[
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 9},
+		{title: 'post 3', upvotes: 3},
+		{title: 'post 4', upvotes: 7},
+		{title: 'post 5', upvotes: 4}
+		]
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts) {
+		$scope.test = "Hello World!";
+		$scope.posts = posts.posts;
+
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === '') { return; }
+				$scope.posts.push({
+					title: $scope.title,
+					link: $scope.link,
+					upvotes: 0,
+					comments : [
+					{author: 'Joe', body: 'cool post bro!', upvotes: 0},
+					{author: 'Jim', body: 'NIceeee!', upvotes: 2}
+					]
+				});
+				$scope.title = '';
+				$scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
+
+	}]);
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function() {
+			if($scope.body === '') { return; }
+				$scope.post.comments.push({
+					body: $scope.body,
+					author: 'user',
+					upvotes: 0
+				});
+			$scope.body = '';		
+		};
+	}]);
